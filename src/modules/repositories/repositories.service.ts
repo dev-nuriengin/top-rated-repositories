554ccc999a85f5433383124a3b23c7fae7550d1e
Nuri Engin <nuri.engin@finbyte.com>
2023-11-28 23:28:40 +0100

    chore: more code tunning
    Ensure to split functionalities into their own scopes, so we can provide more tests, or achieve more readability, etc.

--- src/modules/repositories/repositories.service.ts
+++ src/modules/repositories/repositories.service.ts
@@ -24,52 +24,66 @@ export class RepositoriesService {
   }
 
   async getTopRatedRepositories(date: string, language: string, limit: number) {
-    // INFO: Keep each step into specific variables for now;
-    // Thus, in future we may make it easier to debug. add logs/metrics, and to test
-    // Also, we can split each step into a separate function to provide more business logic
+    const url = this.generateRequestUrl(date);
 
-    // TODO: Get this from config settings
-    const url = `https://raw.githubusercontent.com/EvanLi/Github-Ranking/master/Data/github-ranking-${date}.csv`;
-
-    // TODO: Provide caching mechanism to avoid calling this API multiple times or response same filtered data
-    const config: AxiosRequestConfig = {
-      baseURL: url,
-      method: 'GET',
-      params: {
-        date,
-        language,
-        limit,
-      },
-    };
+    const config = this.generateRequestConfig(url, { date, language, limit });
 
     try {
+      // TODO: Provide caching mechanism to avoid calling this API multiple times or response same filtered data
       const http = this.httpService.request(config);
 
       const response = await firstValueFrom(http.pipe(map((res) => res.data)));
 
-      // TODO: Split this into a separate functions
-      let repositories = await csv().fromString(response);
+      const repositories = await this.parseCsv(response);
 
       if (!repositories) {
         throw new NotFoundException('Repositories not found');
       }
 
-      if (language) {
-        repositories = repositories.filter(
-          (repo) => repo.language === language,
-        );
-      }
-
-      if (limit && limit > 0) {
-        repositories = repositories.slice(0, limit);
-      }
+      const filteredRepositories = await this.filterAndLimitRepositories(
+        repositories,
+        language,
+        limit,
+      );
 
       return {
         status: 'success',
-        result: repositories,
+        result: filteredRepositories,
       };
     } catch (error) {
       throw new InternalServerErrorException('Failed to fetch repositories');
     }
+  }
+
+  // TODO: Get URL from config settings
+  private generateRequestUrl(date: string): string {
+    return `https://raw.githubusercontent.com/EvanLi/Github-Ranking/master/Data/github-ranking-${date}.csv`;
+  }
+
+  // INFO: Here we generate the config for axios but later on we can use a custom http service
+  private generateRequestConfig(url: string, params: any): AxiosRequestConfig {
+    return { baseURL: url, method: 'GET', params };
+  }
+
+  // INFO: Here we encapsulate CSV usage, so later we can easily replace it with another library
+  private async parseCsv(csvData: string): Promise<any[]> {
+    return csv().fromString(csvData);
+  }
+
+  // INFO: Here we can run more complex logic to filter and limit the data, and provide more specific tests
+  private async filterAndLimitRepositories(
+    repositories: any[],
+    language: string,
+    limit: number,
+  ): Promise<any[]> {
+    if (language) {
+      repositories = repositories.filter((repo) => repo.language === language);
+    }
+
+    if (limit && limit > 0) {
+      repositories = repositories.slice(0, limit);
+    }
+
+    return repositories;
   }
 }
